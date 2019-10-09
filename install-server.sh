5fc93922f859da318784a8befb01282f38e9140f
boot1110001 <ppuubblliicc@protonmail.com>
2019-10-09 21:22:53 +0200

    systemd script

---
+++ install-server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# RUN AS ROOT!!
+#apt install openjdk-8-jre-headless curl screen nano bash grep git
+apt install openjdk-* curl screen nano bash grep git
+mkdir -p /opt/minecraft
+adduser --system --shell /bin/bash --home /opt/minecraft --group minecraft
+cd /opt/minecraft
+git clone https://github.com/boot1110001/minesrv.git
+cd /opt/minecraft/minesrv
+cp minecraft@.service /etc/systemd/system/minecraft@.service
+#chown minecraft:root -R /opt/minecraft #no se si esto es bueno o no
+systemctl enable minecraft@minesrv
+#systemctl disable minecraft@minesrv
