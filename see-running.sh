b8a9c6959c13327727a9ab527a8b8831feed5998
boot1110001 <ppuubblliicc@protonmail.com>
2019-10-10 21:58:06 +0200

    fin sh

--- see-running.sh
+++ see-running.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
-screen -ls
-LIST=$(screen -ls | grep mc-)
-echo $LIST
+#screen -ls
+LIST=$(screen -ls | grep mc- | cut -c 2- | cut -f1)
+
+#echo $LIST #list all the sessions
+i=0
+while read -r line; do
+	echo -e "\t$i - $line"
+	i=$((i+1))
+done <<< "$LIST"
+
+#echo ${LIST[0]} #list all the first session
+read -p "[ IN ] Write the number of the session: " num
+if [ $num -gt '-1' ] && [ $num -lt $i ]; then
+	echo "[INFO] Opening: ${LIST["$num"]}..."
+	echo "[INFO] Press Ctrl+A+D to exit."
+	sleep 2
+	screen -r "${LIST["$num"]}"
+else
+	echo "[FAIL] $num isn't a valid session number."
+	exit
+fi
