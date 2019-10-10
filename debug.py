74f9ee9b5a94ad028d803046b7c53b67a547ba2b
boot1110001 <ppuubblliicc@protonmail.com>
2019-10-10 21:09:32 +0200

    wololo

--- debug.py
+++ debug.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import os
-import datetime
+import gzip
+# import datetime
 
-today_str = datetime.datetime.now().strftime('%Y-%m-%d')
-yesterday_str = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+# today_str = datetime.datetime.now().strftime('%Y-%m-%d')
 date_str = ''
 logs_today = False
 files_arr = []
@@ -12,19 +12,22 @@ for root, dirs, files in os.walk("logs"):
     files_arr = files
 
 files_arr.sort()
-files_arr.pop()
+files_arr.pop() # delete the las element (latest.log)
 
-for filename in files_arr:
-    if (today_str in filename):
-        logs_today = True
-
-if logs_today:
-    date_str = today_str
-else:
-    date_str = yesterday_str
+date_str = files_arr[-1][:10]
 
 print("## Lastest logs:")
 
 for filename in files_arr:
     if (date_str in filename):
-        print(filename)
+        # print("## Reading 'logs/"+filename+"'")
+        f = gzip.open('logs/'+filename, 'rb')
+        file_content = f.read()
+        f.close()
+        print(file_content.decode('utf-8'))
+
+# f = gzip.open('logs/2019-10-09-6.log.gz', 'rb')
+# file_content = f.read()
+# f.close()
+# print("## Reading 'logs/2019-10-09-6.log.gz'")
+# print(file_content.decode('utf-8'))
